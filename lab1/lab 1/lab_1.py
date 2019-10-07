5d353d7e197796b9c432bfd5bafeb5f64843d184
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-10-07 23:04:05 +0300

    lab1 example done

--- lab1/lab 1/lab_1.py
+++ lab1/lab 1/lab_1.py
@@ -3,16 +3,23 @@ import matplotlib.pyplot as plt
 import skimage.data
 import cv2
 
-image=np.array(skimage.data.coins())
-#print(image)
+image=np.array(object=skimage.data.coins(), dtype=np.uint8)
 fig, ax = plt.subplots()
-ax.imshow(image, cmap='Greys_r')
+ax.imshow(image)#, cmap='Greys_r')
 
 
 image[image<128]=0
 image[image>=128]=1
 fig, ax = plt.subplots()
 ax.imshow(image, cmap='Greys_r')
+
+img2=cv2.morphologyEx(src=image, op=cv2.MORPH_CLOSE, kernel=np.array([[1]*9]*9))
+fig, ax = plt.subplots()
+ax.imshow(img2, cmap='Greys_r')
+
+img3=cv2.erode(src=img2, kernel=np.array([[1]*3]*3))
+fig, ax = plt.subplots()
+ax.imshow(img3, cmap='Greys_r')
 
 plt.show()
 
