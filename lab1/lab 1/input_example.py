9717116c9614708659d35be7437b1445d43c7155
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-11-18 09:35:45 +0300

    lab1 final

--- lab1/lab 1/input_example.py
+++ lab1/lab 1/input_example.py
@@ -15,9 +15,11 @@ print(image)
 fig, ax = plt.subplots()
 ax.imshow(image, cmap='Greys_r')
 
+BORDER=200
+
 image=np.average(image, axis=2)
-image[image<128]=0
-image[image>=128]=1
+image[image<BORDER]=0
+image[image>=BORDER]=1
 fig, ax = plt.subplots()
 ax.imshow(image, cmap='Greys_r')
 
@@ -25,7 +27,15 @@ img2=cv2.morphologyEx(src=image, op=cv2.MORPH_CLOSE, kernel=np.array([[1]*9]*9))
 fig, ax = plt.subplots()
 ax.imshow(img2, cmap='Greys_r')
 
-img3=cv2.erode(src=img2, kernel=np.array([[1]*3]*3))
+# img3=cv2.dilate(src=img2, kernel=np.array([[1]*9]*9))
+# fig, ax = plt.subplots()
+# ax.imshow(img3, cmap='Greys_r')
+
+img3=cv2.erode(src=img2, kernel=np.array([[1]*9]*9))
+fig, ax = plt.subplots()
+ax.imshow(img3, cmap='Greys_r')
+
+img3=cv2.dilate(src=img2, kernel=np.array([[1]*9]*9))
 fig, ax = plt.subplots()
 ax.imshow(img3, cmap='Greys_r')
 
