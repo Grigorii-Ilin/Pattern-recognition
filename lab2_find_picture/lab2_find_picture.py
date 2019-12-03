06353ccb2f8da01e3f5629615c9962e45acea7da
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-12-03 12:20:22 +0300

    lab2 done

--- lab2_find_picture/lab2_find_picture.py
+++ lab2_find_picture/lab2_find_picture.py
@@ -1,7 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
-#import numpy
-import glob#, os
+import glob
 
 work_dir=r"C:\MY_DOC_HP\BMSTU\2019_2\Pattern-recognition\adobe_panoramas\data"
 
@@ -13,29 +12,34 @@ orig_img_with_key_points = cv2.drawKeypoints(orig_image, orig_key_points, None)
 plt.imshow(orig_img_with_key_points)
 plt.show()
 
-#os.chdir(r"C:\MY_DOC_HP\BMSTU\2019_2\Pattern-recognition\adobe_panoramas\data\carmel")
 best_matches_count=0
 best_file=""
+figure=plt.figure(figsize=(2,2))
 for filename in glob.glob(work_dir+r"\*\*.png"):
 
     cur_img = cv2.imread(filename)
     cur_img = cv2.cvtColor(cur_img, cv2.COLOR_BGR2GRAY)
-    #(kp, des) = sift.detectAndCompute(image, None)
     cur_key_points, cur_descriptions = sift.detectAndCompute(cur_img, None)
-    #i = cv2.drawKeypoints(orig_image, orig_key_points, None)
     knn_match = cv2.BFMatcher().knnMatch(orig_descriptions, cur_descriptions, k=2)
-    #matches = match.knnMatch(orig_descriptions, cur_descriptions, k=2)
-    #good_matches = numpy.array([m1 for m1, m2 in matches if m1.distance < m2.distance / 2])
 
-    cur_matches_count=len([True for m1, m2 in knn_match if m1.distance<m2.distance/2])
+    good_matches=[m1 for m1, m2 in knn_match if m1.distance<m2.distance/2]
+    cur_matches_count=len(good_matches)
 
-    # img3 = cv2.drawMatches(image, kp, image2, kp2, good_matches, None)
-    # if good_matches.size > 100:
-    #    # plt.imshow(img3)
-    #    # plt.show()
-    #    image = image2
-
-    #    print(file, good_matches.size)
+    if cur_matches_count >= 10:
+        draw_matches=cv2.drawMatches(orig_image, 
+                                    orig_key_points,
+                                    cur_img, 
+                                    cur_key_points,
+                                    good_matches,
+                                    None)
+               
+        figure.add_subplot(2,2,1)
+        plt.imshow(orig_image)
+        figure.add_subplot(2,2,2)
+        plt.imshow(cur_img)
+        figure.add_subplot(2,2,3)
+        plt.imshow(draw_matches)
+        plt.show()
 
     if cur_matches_count>best_matches_count:
         best_matches_count=cur_matches_count
@@ -43,4 +47,4 @@ for filename in glob.glob(work_dir+r"\*\*.png"):
 
     print(filename, cur_matches_count)
 
-print("Лучшее совпадение:", best_file, best_matches_count)
+print("\nЛучшее совпадение:", best_file, best_matches_count)
